6201c5458478f97252b806319587670968c05d9e
Noah Brauner <nbbrauner@gmail.com>
2020-11-20 14:10:16 -0500

    Initial Commit

---
+++ MagneticPoetry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MagneticPoetry
+//
+//  Created by Noah Brauner on 11/20/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MagneticPoetry/MagneticPoetryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagneticPoetryApp.swift
+//  MagneticPoetry
+//
+//  Created by Noah Brauner on 11/20/20.
+//
+
+import SwiftUI
+
+@main
+struct MagneticPoetryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
